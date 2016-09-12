f5269cc96d058c1e1fd936665658110bfdefcc32
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-09-12 01:55:41 -0500

    Project 1: Search in Pacman

--- pacman-search/search.py
+++ pacman-search/search.py
@@ -18,7 +18,7 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
-from util import Stack, Queue
+from util import Stack, Queue, PriorityQueue
 
 class SearchProblem:
     """
@@ -135,7 +135,7 @@ def breadthFirstSearch(problem):
     back_tracked_tuple = goal_tuple
 
     while back_tracked_tuple[1] is not None:
-        actions_stack.push(back_tracked_tuple[1]);
+        actions_stack.push(back_tracked_tuple[1])
         for visited_state_tuple in visited_state_tuples:
             if visited_state_tuple[0] == back_tracked_tuple:
                 back_tracked_tuple = visited_state_tuple[1]
@@ -155,8 +155,8 @@ def breadthFirstSearchInner(problem, visited_state_tuples, fringe_queue):
         current_state = states_tuple[0][0]
 
         if current_state not in [visited_state_tuple[0][0] for visited_state_tuple in visited_state_tuples]:
-
             visited_state_tuples.add(states_tuple)
+
             if problem.isGoalState(current_state):
                 print "goal state :", current_state
                 return states_tuple[0]
@@ -164,10 +164,11 @@ def breadthFirstSearchInner(problem, visited_state_tuples, fringe_queue):
             successors = problem.getSuccessors(current_state)
 
             if len(successors) > 0:
-                for next_successor in successors:
-                    if next_successor[0] not in \
+                for successor in successors:
+                    if successor[0] not in \
                             [visited_state_tuple[0][0] for visited_state_tuple in visited_state_tuples]:
-                        future_fringe_queue.push((next_successor, states_tuple[0]))
+                        # add parent-child pair that will help to backtrack
+                        future_fringe_queue.push((successor, states_tuple[0]))
 
     if len(future_fringe_queue.list) > 0:
         return breadthFirstSearchInner(problem, visited_state_tuples, future_fringe_queue)
@@ -177,8 +178,60 @@ def breadthFirstSearchInner(problem, visited_state_tuples, fringe_queue):
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # this will contain child-parent successor pair
+    visited_state_tuples = set()
+
+    # we have to use PriorityQueue, which is a min-heap and keep minimum value node on the top.
+    fringe_queue = PriorityQueue()
+
+    # element structure : current node, parent node, effective cost
+    fringe_queue.push(((problem.getStartState(), None, 0), None, 0), 0)
+
+    # execute UFS
+    goal_tuple = uniformCostSearchInner(problem, visited_state_tuples, fringe_queue)
+    print "goal_tuple :", goal_tuple
+
+    actions_stack = Stack()
+
+    # back tracking to build action stack, will be build bottom-up.
+    back_tracked_tuple = goal_tuple
+
+    while back_tracked_tuple[1] is not None:
+        actions_stack.push(back_tracked_tuple[1])
+        for visited_state_tuple in visited_state_tuples:
+            if visited_state_tuple[0] == back_tracked_tuple:
+                back_tracked_tuple = visited_state_tuple[1]
+                break
+
+    # reverse the action stack
+    actions_stack = actions_stack.list[::-1]
+    print "actions path :", actions_stack
+    return actions_stack
+
+
+def uniformCostSearchInner(problem, visited_state_tuples, fringe_queue):
+
+    while len(fringe_queue.heap) > 0:
+        states_tuple = fringe_queue.pop()
+        current_state = states_tuple[0][0]
+
+        if current_state not in [visited_state_tuple[0][0] for visited_state_tuple in visited_state_tuples]:
+            visited_state_tuples.add(states_tuple)
+
+            if problem.isGoalState(current_state):
+                print "goal state :", current_state
+                return states_tuple[0]
+
+            successors = problem.getSuccessors(current_state)
+
+            if len(successors) > 0:
+                for successor in successors:
+                    if successor[0] not in \
+                            [visited_state_tuple[0][0] for visited_state_tuple in visited_state_tuples]:
+                        # add parent-child pair that will help to backtrack
+                        fringe_queue.push((successor, states_tuple[0], states_tuple[0][2] + successor[2]), states_tuple[2] + successor[2])
+
+    return None
 
 
 def nullHeuristic(state, problem=None):
